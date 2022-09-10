b0bcf548af8e4c67f0a992e5f7daea54192406e6
Jean <jeanduarte322@outlook.com>
2022-09-10 13:50:51 -0300

    Updates

---
+++ precache-manifest.3cbeb7a9ee7d7664d31249fc7dead8b8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "a6ac11089b89b5bc4b00",
+    "url": "/login-page-example/static/css/main.0a9faedc.chunk.css"
+  },
+  {
+    "revision": "a6ac11089b89b5bc4b00",
+    "url": "/login-page-example/static/js/main.a6ac1108.chunk.js"
+  },
+  {
+    "revision": "78450bd5e302d1852f63",
+    "url": "/login-page-example/static/js/1.78450bd5.chunk.js"
+  },
+  {
+    "revision": "808a2ac566f045dce11f",
+    "url": "/login-page-example/static/js/runtime~main.808a2ac5.js"
+  },
+  {
+    "revision": "e017ab3444b2a792453045915516f73c",
+    "url": "/login-page-example/static/media/shortfilm.e017ab34.MOV"
+  },
+  {
+    "revision": "9d365594a60d760d11236c57195fb05c",
+    "url": "/login-page-example/static/media/crey.9d365594.jpg"
+  },
+  {
+    "revision": "de68e1bbf4eca794969bf622639e9178",
+    "url": "/login-page-example/index.html"
+  }
+];
