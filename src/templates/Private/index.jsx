7a3a677e984ff98be21999a98f37025ae114ebe4
Jean <jeanduarte322@outlook.com>
2022-09-10 17:23:15 -0300

    Adicionando o redirecionamento

--- src/templates/Private/index.jsx
+++ src/templates/Private/index.jsx
@@ -1,20 +1,37 @@
 import React, { useContext } from "react";
+import { Navigate } from "react-router-dom";
 import { AuthContext } from "../../contexts/Auth";
 
-import './styles.css'
+import "./styles.css";
 
 export const Private = () => {
   const auth = useContext(AuthContext);
-  alert("Privado success")
+  alert("Privado success");
+
   const handleClick = () => {
     auth.signout();
+    return <Navigate to="/" />;
   };
 
   return (
     <div className="container-page-private">
-      <h1 style={{textAlign:"center",fontSize:"4.5rem",fontFamily:"Genos"}}>Pagina privada!!</h1>
-      <button onClick={handleClick} className="button-page-private">Logout</button>
-      <a className="github-button" href="https://github.com/Jeanduarty/login-page-example" target="_blank">GitHub</a>
+      <h1
+        style={{ textAlign: "center", fontSize: "4.5rem", fontFamily: "Genos" }}
+      >
+        Pagina privada!!
+      </h1>
+      <button onClick={handleClick} className="button-page-private">
+        Logout
+      </button>
+      <a
+        className="github-button"
+        href="https://github.com/Jeanduarty/login-page-example"
+        target="_blank"
+        without
+        rel="noopener noreferrer"
+      >
+        GitHub
+      </a>
     </div>
   );
 };
